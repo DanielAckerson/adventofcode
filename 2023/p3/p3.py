9befbf4844e1941f240dd22ab049ded24c6654d4
Daniel Ackerson <dackerson0@gmail.com>
2023-12-03 22:54:37 -0500

    day 3 - part one, second attempt

--- 2023/p3/p3.py
+++ 2023/p3/p3.py
@@ -1,74 +1,118 @@
 #! /usr/bin/env python3
 
+import re
 from argparse import ArgumentParser
 from collections.abc import Iterator
 from pathlib import Path
 
 
-SYMBOLS = '#$%&*+-/=@'
-NUMBERS = '0123456789'
-EMPTY = '.'
-CHARACTERS = set(SYMBOLS + NUMBERS + EMPTY)
+#SYMBOLS = '#$%&*+-/=@'
+#NUMBERS = '0123456789'
+#EMPTY = '.'
+#CHARACTERS = set(SYMBOLS + NUMBERS + EMPTY)
+#
+#
+#class EngineSchematic:
+#    def __init__(self, path: Path):
+#        self.path = path
+#        self.schematic, self.width, self.height = self.read_schematic(path)
+#
+#    def get(self, x: int, y: int) -> str:
+#        if x < 0 or x >= self.width or y < 0 or y >= self.height:
+#            return ''
+#        return self.schematic[x + y * self.width]
+#
+#    def iter_cells(self) -> Iterator[tuple[str, int, int]]:
+#        for y in range(0, self.height):
+#            for x in range(0, self.width):
+#                yield self.get(x, y), x, y
+#
+#    def iter_neighbors(self, x: int, y: int) -> Iterator[tuple[str, int, int]]:
+#        for cury in range(y - 1, y + 2):
+#            for curx in range(x - 1, x + 2):
+#                if curx == x and cury == y:
+#                    continue
+#                yield self.get(curx, cury), curx, cury
+#                    
+#    @classmethod
+#    def read_schematic(cls, path: Path) -> tuple[str, int, int]:
+#        with path.open('r') as schematic_file:
+#            schematic = schematic_file.readline().strip()
+#
+#            if (line_len := len(schematic)) == 0:
+#                raise RuntimeError('Empty schematic file')
+#
+#            width = line_len
+#            height = 1
+#
+#            while line := schematic_file.readline().strip():
+#                assert len(line) == width
+#                schematic += line
+#                height += 1
+#
+#            return schematic, width, height
+#
+#
+#def is_number(cell: str) -> bool:
+#    return bool(cell in NUMBERS)
+#
+#
+#def is_symbol(cell: str) -> bool:
+#    return bool(cell in SYMBOLS)
+#
+#
+#def is_part_number(cell: str, engine_schematic: EngineSchematic, posx: int, posy: int) -> bool:
+#    # if any of 8 neighbors of a cell is a symbol and if the cell is a number, then it is a part number
+#    return is_number(cell) and any(c for c, _, _ in engine_schematic.iter_neighbors(posx, posy) if is_symbol(c))
+#
+
+def symbol_adjacent_to_number(num_match: re.Match, sym_match: re.Match) -> bool:
+    ...
 
 
-class EngineSchematic:
-    def __init__(self, path: Path):
-        self.path = path
-        self.schematic, self.width, self.height = self.read_schematic(path)
+def load_schematic(path: Path) -> tuple[dict, dict]:
+    symbols = dict[tuple[int, int], re.Match]()
+    numbers = dict[tuple[int, int], re.Match]()
 
-    def get(self, x: int, y: int) -> str:
-        if x < 0 or x >= self.width or y < 0 or y >= self.height:
-            return ''
-        return self.schematic[x + y * self.width]
+    y = 0
 
-    def iter_cells(self) -> Iterator[tuple[str, int, int]]:
-        for y in range(0, self.height):
-            for x in range(0, self.width):
-                yield self.get(x, y), x, y
+    with path.open('r') as eng_file:
+        for line in eng_file.readlines():
+            for sym in re.finditer('[-+#$%&*/=@]', line):
+                symbols[(sym.span()[0], y)] = sym
 
-    def iter_neighbors(self, x: int, y: int) -> Iterator[tuple[str, int, int]]:
-        for cury in range(y - 1, y + 2):
-            for curx in range(x - 1, x + 2):
-                if curx == x and cury == y:
-                    continue
-                yield self.get(curx, cury), curx, cury
-                    
-    @classmethod
-    def read_schematic(cls, path: Path) -> tuple[str, int, int]:
-        with path.open('r') as schematic_file:
-            schematic = schematic_file.readline().strip()
+            for num in re.finditer('\d+', line):
+                numbers[(num.span()[0], y)] = num
 
-            if (line_len := len(schematic)) == 0:
-                raise RuntimeError('Empty schematic file')
-
-            width = line_len
-            height = 1
-
-            while line := schematic_file.readline().strip():
-                assert len(line) == width
-                schematic += line
-                height += 1
-
-            return schematic, width, height
+    return symbols, numbers
 
 
-def is_number(cell: str) -> bool:
-    return bool(cell in NUMBERS)
+def iter_neighbors(posx: int, posy: int) -> Iterator[tuple[int, int]]:
+    for y in range(posy - 1, posy + 2):
+        for x in range(posx - 1, posx + 2):
+            if posx == x and posy == y:
+                continue
+            yield x, y
 
 
-def is_symbol(cell: str) -> bool:
-    return bool(cell in SYMBOLS)
+def has_symbol_neighbor(posx: int, posy: int, symbols: dict[tuple[int, int], re.Match]) -> bool:
+    for nx, ny in iter_neighbors(posx, posy):
+        if symbols.get((nx, ny), None):
+            return True
+    return False
 
 
-def is_part_number(cell: str, engine_schematic: EngineSchematic, posx: int, posy: int) -> bool:
-    # if any of 8 neighbors of a cell is a symbol and if the cell is a number, then it is a part number
-    return is_number(cell) and any(c for c, _, _ in engine_schematic.iter_neighbors(posx, posy) if is_symbol(c))
+def iter_part_numbers(symbols: dict[tuple[int, int], re.Match], numbers: dict[tuple[int, int], re.Match]) -> Iterator[int]:
+    for (_, y), num in numbers.items():
+        for x in range(*num.span()):
+            if has_symbol_neighbor(x, y, symbols):
+                yield int(num[0])
 
 
-def solution_part_one(engine_schematic: EngineSchematic) -> int:
-    return sum(int(c) for c,*_ in filter(
-        lambda c: is_part_number(c[0], engine_schematic, c[1], c[2]),
-        engine_schematic.iter_cells()))
+def solution_part_one(path: Path) -> int:
+    symbols, numbers = load_schematic(path)
+    return sum(iter_part_numbers(symbols, numbers))
+
 
 
 def main():
@@ -78,9 +122,10 @@ def main():
 
     args = parser.parse_args()
     engine_schematic_path = args.path
-    engine = EngineSchematic(engine_schematic_path)
+    #engine = EngineSchematic(engine_schematic_path)
 
-    print(f'solution 1: {solution_part_one(engine)}')
+    #print(f'solution 1: {solution_part_one(engine)}')
+    print(f'solution 1: {solution_part_one(engine_schematic_path)}')
 
     ## test EngineSchematic.get()
     #for y in range(engine.height):
